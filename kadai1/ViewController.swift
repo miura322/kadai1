8328c229279457e2545484995a546c99601266ed
miura322 <afgl_mk93@icloud.com>
2021-10-12 15:14:50 +0900

    commit

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -7,19 +7,13 @@
 
 import UIKit
 
-extension UITextField {
-    var textToInt: Int {
-        let text = self.text
-        let int = text.flatMap {Int($0)} ?? 0
-        return int
-    }
-}
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
     @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
     @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
@@ -28,11 +22,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculate(_ sender: Any) {
-        let num1 = textField1.textToInt
-        let num2 = textField2.textToInt
-        let num3 = textField3.textToInt
-        let num4 = textField4.textToInt
-        let calc = num1 + num2 + num3 + num4
+        let num1 = Int(textField1.text!) ?? 0
+        let num2 = Int(textField2.text!) ?? 0
+        let num3 = Int(textField3.text!) ?? 0
+        let num4 = Int(textField4.text!) ?? 0
+        let num5 = Int(textField5.text!) ?? 0
+        let calc = num1 + num2 + num3 + num4 + num5
+
         label.text = "\(calc)"
     }
 
