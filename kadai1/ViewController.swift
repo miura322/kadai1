2eccd4cce7d24381db4b99bc860ef5132a032626
akio0911 <akio0911@gmail.com>
2021-10-13 16:33:08 +0900

    feedback

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -9,17 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var textField3: UITextField!
-    @IBOutlet weak var textField4: UITextField!
-    @IBOutlet weak var textField5: UITextField!
-    @IBOutlet weak var label: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
+    @IBOutlet private weak var label: UILabel!
 
     @IBAction func calculate(_ sender: Any) {
         let num1 = Int(textField1.text!) ?? 0
@@ -27,9 +22,9 @@ class ViewController: UIViewController {
         let num3 = Int(textField3.text!) ?? 0
         let num4 = Int(textField4.text!) ?? 0
         let num5 = Int(textField5.text!) ?? 0
-        let calc = num1 + num2 + num3 + num4 + num5
+        let total = num1 + num2 + num3 + num4 + num5
 
-        label.text = "\(calc)"
+        label.text = "\(total)"
     }
 
 }
